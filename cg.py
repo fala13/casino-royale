3e5730fe36679eb608166be07032bb0bd5b10f1c
Michal Falenski <sket_fala@o2.pl>
2018-04-28 21:59:50 +0200

    Silver reached

--- cg.py
+++ cg.py
@@ -2,6 +2,10 @@ import sys
 import math
 import functools
 
+UT = True
+UT = False
+
+
 def dist4(x1, y1, x2, y2):
    # print(str(x1) + " " + str(y1) + " " + str(x2) + " "+ str(y2) +" " +str( math.sqrt((x1-x2)**2 + (y1-y2)**2)), file=sys.stderr)
     return math.sqrt((x1-x2)**2 + (y1-y2)**2)
@@ -13,31 +17,40 @@ def dist(a, b):
 
 def find_nearest(q, places):
  #   assert (type(places) is dict)
-    print("queen at " + str(q['x']) + " " + str(q['y']), file=sys.stderr)
-    return min(places, key=lambda k: dist(q, places[k]))
+ #   print("queen at " + str(q['x']) + " " + str(q['y']), file=sys.stderr)
+    return min(places, key=lambda k: dist(q, k))
 
-
-num_sites = int(input())
-#num_sites = 0
-sites = {}
-buildings = {}
+if not UT:
+    num_sites = int(input())
+else:
+    num_sites = 0
+sites = []
 objects = []
+touched_site = 0
+gold_left = [-2] * num_sites
 for i in range(num_sites):
     site_id, x, y, radius = [int(j) for j in input().split()]
-    sites[site_id] = {'site_id': site_id, 'x': x, 'y': y, 'radius': radius}
+    sites.insert(site_id, {'site_id': site_id, 'x': x, 'y': y, 'radius': radius})
+
+def get_queens(objects):
+    queenM = next(q for q in objects if q['unit_type'] == -1 and q['owner'] == 0)
+    queenE = next(q for q in objects if q['unit_type'] == -1 and q['owner'] == 1)
+    return queenM, queenE
 
 
 class Strategy:
-    def get_strategy(self, sites, objects, queenM, queenE):
+    def get_strategy(self, sites, objects):
         print("BUILD 1 BARRACKS-KNIGHT")
         print("TRAIN 1")
         return
 
 
 class Angry(Strategy):
-    def get_strategy(self, sites, objects, queenM, queenE):
-        mine = [v for k, v in sites.items() if v['owner'] == 0]
-        empty_sites = {k:v for k, v in sites.items() if v['owner'] != 0 and v['structure_type'] != 1}
+    def get_strategy(self, sites, objects):
+        queenM, queenE = get_queens(objects)
+
+        mine = [v for  v in sites if v['owner'] == 0]
+        empty_sites = [v for v in sites if v['owner'] != 0 and v['structure_type'] != 1]
 
     #   mine_sort = sorted(mine, key=lambda k: dist(queenE, mine[k]))
         mine_sort = sorted(mine, key=lambda k: dist(queenE, k))
@@ -52,22 +65,103 @@ class Angry(Strategy):
         return
 
 
-class Suicide(Strategy):
-    def get_strategy(self, sites, objects, queenM, queenE):
-        mine = [v for k, v in sites.items() if v['owner'] == 0]
-        empty_sites = {k:v for k, v in sites.items() if v['owner'] != 0 and v['structure_type'] != 1}
+class Situation():
+    def __init__(self, sites, objects):
+        self.sites = sites
+        self.objects = objects
 
-        #   mine_sort = sorted(mine, key=lambda k: dist(queenE, mine[k]))
-        mine_sort = sorted(mine, key=lambda k: dist(queenE, k))
 
-        barrack_count = functools.reduce(lambda a, x: a+1 if x['owner'] == 0 and x['structure_type'] == 2 else a,
-                                         sites.items(),
+class CounterSituation(Situation):
+    def __init__(self, sites, objects):
+        self.sites = sites
+        self.objects = objects
+        queenM, queenE = get_queens(objects)
+
+        owned_sites = filter(lambda x: x['owner'] == 0, sites)
+        self.mbarracks = functools.reduce(lambda a, x: a+1 if x['structure_type'] == 2 else a,
+                                         owned_sites,
                                          0)
 
-        if barrack_count > 2:
-            print("BUILD " + str(find_nearest(queenM, empty_sites)) + " TOWER")
+        self.mine_rate = functools.reduce(lambda a, x: a+x['param_1'] if x['structure_type'] == 0 else a, sites, 0)
+        #print(str(self.mine_rate),file=sys.stderr)
+
+        #self.enemy_near = [] == next(True for q in objects if q['owner'] == 1 and (dist(queenM, q) < 300))
+        #self.enemy_near = [] == next(True for q in objects if q['owner'] == 1 and dist(queenM, q) < 300)
+        self.enemy_near = [] != [True for q in objects if q['owner'] == 1 and dist(queenM, q) < 100]
+        print(str(self.enemy_near), file=sys.stderr)
+
+def dt(cond, l, r):
+    if cond:
+        return l
+    else:
+        return r
+
+class DTS(Strategy):
+    def get_strategy(self, sites, objects):
+        queenM, queenE = get_queens(objects)
+
+        sit = CounterSituation(sites, objects)
+
+        mine = [v for v in sites if v['owner'] == 0]
+        mine_kbaracks = [v for v in sites if v['owner'] == 0 and v['structure_type'] == 2 and v['param_2'] == 0]
+        empty_sites = [v for v in sites if v['owner'] != 0 and v['structure_type'] != 1]
+
+        #print(str(empty_sites), file=sys.stderr)
+        #   mine_sort = sorted(mine, key=lambda k: dist(queenE, mine[k]))
+        mine_sort = sorted(mine_kbaracks, key=lambda k: dist(queenE, k))
+
+        s_id, build = dt(sites[touched_site]['structure_type'] == 1 and sites[touched_site]['param_1'] < 700 and sites[touched_site]['owner'] == 0,
+                         (touched_site, 1),
+                         dt(not sit.enemy_near and sites[touched_site]['structure_type'] == 0 and sites[touched_site]['param_1'] < sites[touched_site]['maxMineSize'] and sites[touched_site]['gold'] > 5,
+                            (touched_site, 0),
+                            dt(not sit.enemy_near and sit.mine_rate < 3 and empty_sites,
+                               (find_nearest(queenM, filter(lambda x: x['gold'] == -2 or x['gold'] > 5, empty_sites))['site_id'], 0),
+                               dt(sit.mbarracks > 0,
+                                  (find_nearest(queenM, empty_sites)['site_id'], 1),
+                                  (find_nearest(queenM, empty_sites)['site_id'], 2)
+                                  )
+                               )
+                            )
+                         )
+
+        building = "MINE"
+        if build == 1:
+            building = "TOWER"
+        elif build == 2:
+            building = "BARRACKS-KNIGHT"
+
+        print("BUILD " + str(s_id) + " " + building)
+
+        if mine_sort:
+            print("TRAIN " + str(mine_sort[0]['site_id']))
         else:
-            print("BUILD " + str(find_nearest(queenM, empty_sites)) + " BARRACKS-KNIGHT")
+            print("TRAIN 1")
+        return
+
+
+class Suicide(Strategy):
+    def get_strategy(self, sites, objects):
+        queenM, queenE = get_queens(objects)
+
+        sit = CounterSituation(sites, objects)
+
+        mine = [v for v in sites if v['owner'] == 0]
+        mine_kbaracks = [v for v in sites if v['owner'] == 0 and v['structure_type'] == 2 and v['param_2'] == 0]
+        empty_sites = [v for v in sites if v['owner'] != 0 and v['structure_type'] != 1]
+
+        #   mine_sort = sorted(mine, key=lambda k: dist(queenE, mine[k]))
+        mine_sort = sorted(mine_kbaracks, key=lambda k: dist(queenE, k))
+
+        #if sites[touched_site]['structure_type'] == 0 and sites[touched_site]['maxMineSize'] < sites[touched_site]['maxMineSize']:
+         #   print("BUILD " + str(touched_site) + " MINE")
+
+  #      if sit.mine_rate < 5:
+ #           print("BUILD " + str(find_nearest(queenM, empty_sites)['site_id']) + " MINE")
+
+        if sit.mbarracks > 2:
+            print("BUILD " + str(find_nearest(queenM, empty_sites)['site_id']) + " TOWER")
+        else:
+            print("BUILD " + str(find_nearest(queenM, empty_sites)['site_id']) + " BARRACKS-KNIGHT")
 
         if mine:
             print("TRAIN " + str(mine_sort[0]['site_id']))
@@ -75,32 +169,36 @@ class Suicide(Strategy):
             print("TRAIN 1")
         return
 
-strat = Suicide()
+
+strat = DTS()
 # game loop
-while True:
-#while False:
+while True and not UT:
     # touched_site: -1 if none
     gold, touched_site = [int(i) for i in input().split()]
     for i in range(num_sites):
         # structure_type: -1 = No structure, 2 = Barracks
         # owner: -1 = No structure, 0 = Friendly, 1 = Enemy
         site_id, ignore_1, ignore_2, structure_type, owner, param_1, param_2 = [int(j) for j in input().split()]
-        buildings[site_id] = {'site_id': site_id, 'structure_type': structure_type, 'owner':owner}
+        if ignore_1 == -1:
+            sites[site_id]['gold'] = gold_left[site_id]
+        else:
+            sites[site_id]['gold'] = ignore_1
+            gold_left[site_id] = ignore_1
+
+        sites[site_id]['maxMineSize'] = ignore_2
         sites[site_id]['structure_type'] = structure_type
         sites[site_id]['owner'] = owner
-        sites[site_id]['left'] = param_1
+        sites[site_id]['param_1'] = param_1
+        sites[site_id]['param_2'] = param_2
     num_units = int(input())
     for i in range(num_units):
         # unit_type: -1 = QUEEN, 0 = KNIGHT, 1 = ARCHER
         x, y, owner, unit_type, health = [int(j) for j in input().split()]
         objects.append({'x':x, 'y': y, 'owner': owner, 'unit_type': unit_type, 'health': health})
-        print(str(objects), file=sys.stderr)
-        if unit_type == -1 and owner== 0:
-            queenM = {'x':x, 'y':y, 'health':health}
-        if unit_type == -1 and owner== 1:
-            queenE = {'x':x, 'y':y, 'health':health}
+        #print(str(objects), file=sys.stderr)
 
-    print (str(sites), file=sys.stderr)
-    print (str(objects), file=sys.stderr)
+    #print(str(sites).replace("}","}\n") + "\n", file=sys.stderr)
+    #print(str(objects).replace("}","}\n") + "\n", file=sys.stderr)
 
-    strat.get_strategy(sites, objects, queenM, queenE)
+
+    strat.get_strategy(sites, objects)
--- ut.py
+++ ut.py
@@ -1,5 +1,13 @@
 import unittest
+from unittest.mock import patch
+
 import cg
+
+objectsD = [{'x': 252, 'y': 252, 'owner': 0, 'unit_type': -1, 'health': 60}, {'x': 1668, 'y': 748, 'owner': 1, 'unit_type': -1, 'health': 60}]
+sitesD = [{'site_id': 0, 'x': 403, 'y': 827, 'radius': 83, 'gold': -1, 'maxMineSize': -1, 'structure_type': -1, 'owner': -1, 'param_1': -1, 'param_2': -1}, {'site_id': 1, 'x': 1517, 'y': 173, 'radius': 83, 'gold': -1, 'maxMineSize': -1, 'structure_type': -1, 'owner': -1, 'param_1': -1, 'param_2': -1}]
+
+objectsX = [{'site_id': 0, 'x': 403, 'y': 827, 'radius': 83, 'gold': -1, 'maxMineSize': -1, 'structure_type': -1, 'owner': -1, 'param_1': -1, 'param_2': -1}, {'site_id': 1, 'x': 1517, 'y': 173, 'radius': 83, 'gold': -1, 'maxMineSize': -1, 'structure_type': 1, 'owner': 1, 'param_1': 344, 'param_2': 341}, {'site_id': 2, 'x': 1042, 'y': 598, 'radius': 60, 'gold': -1, 'maxMineSize': -1, 'structure_type': -1, 'owner': -1, 'param_1': -1, 'param_2': -1}, {'site_id': 3, 'x': 878, 'y': 402, 'radius': 60, 'gold': -1, 'maxMineSize': -1, 'structure_type': -1, 'owner': -1, 'param_1': -1, 'param_2': -1}, {'site_id': 4, 'x': 160, 'y': 725, 'radius': 70, 'gold': 242, 'maxMineSize': 1, 'structure_type': -1, 'owner': -1, 'param_1': -1, 'param_2': -1}, {'site_id': 5, 'x': 1760, 'y': 275, 'radius': 70, 'gold': -1, 'maxMineSize': -1, 'structure_type': 1, 'owner': 1, 'param_1': 376, 'param_2': 352}, {'site_id': 6, 'x': 619, 'y': 439, 'radius': 82, 'gold': 251, 'maxMineSize': 4, 'structure_type': 2, 'owner': 0, 'param_1': 0, 'param_2': 0}, {'site_id': 7, 'x': 1301, 'y': 561, 'radius': 82, 'gold': -1, 'maxMineSize': -1, 'structure_type': 2, 'owner': 1, 'param_1': 0, 'param_2': 1}, {'site_id': 8, 'x': 175, 'y': 445, 'radius': 85, 'gold': 221, 'maxMineSize': 1, 'structure_type': 2, 'owner': 0, 'param_1': 0, 'param_2': 0}, {'site_id': 9, 'x': 1745, 'y': 555, 'radius': 85, 'gold': -1, 'maxMineSize': -1, 'structure_type': 0, 'owner': 1, 'param_1': -1, 'param_2': -1}, {'site_id': 10, 'x': 453, 'y': 204, 'radius': 90, 'gold': 98, 'maxMineSize': 3, 'structure_type': 0, 'owner': 0, 'param_1': 3, 'param_2': -1}, {'site_id': 11, 'x': 1467, 'y': 796, 'radius': 90, 'gold': -1, 'maxMineSize': -1, 'structure_type': 0, 'owner': 1, 'param_1': -1, 'param_2': -1}, {'site_id': 12, 'x': 172, 'y': 172, 'radius': 82, 'gold': 130, 'maxMineSize': 2, 'structure_type': 0, 'owner': 0, 'param_1': 2, 'param_2': -1}, {'site_id': 13, 'x': 1748, 'y': 828, 'radius': 82, 'gold': -1, 'maxMineSize': -1, 'structure_type': 2, 'owner': 1, 'param_1': 0, 'param_2': 0}, {'site_id': 14, 'x': 942, 'y': 836, 'radius': 74, 'gold': -1, 'maxMineSize': -1, 'structure_type': -1, 'owner': -1, 'param_1': -1, 'param_2': -1}, {'site_id': 15, 'x': 978, 'y': 164, 'radius': 74, 'gold': -1, 'maxMineSize': -1, 'structure_type': -1, 'owner': -1, 'param_1': -1, 'param_2': -1}, {'site_id': 16, 'x': 1187, 'y': 820, 'radius': 80, 'gold': -1, 'maxMineSize': -1, 'structure_type': -1, 'owner': -1, 'param_1': -1, 'param_2': -1}, {'site_id': 17, 'x': 733, 'y': 180, 'radius': 80, 'gold': -1, 'maxMineSize': -1, 'structure_type': -1, 'owner': -1, 'param_1': -1, 'param_2': -1}, {'site_id': 18, 'x': 1522, 'y': 439, 'radius': 75, 'gold': -1, 'maxMineSize': -1, 'structure_type': 0, 'owner': 1, 'param_1': -1, 'param_2': -1}, {'site_id': 19, 'x': 398, 'y': 561, 'radius': 75, 'gold': 195, 'maxMineSize': 3, 'structure_type': -1, 'owner': -1, 'param_1': -1, 'param_2': -1}, {'site_id': 20, 'x': 1249, 'y': 177, 'radius': 84, 'gold': -1, 'maxMineSize': -1, 'structure_type': 1, 'owner': 1, 'param_1': 324, 'param_2': 331}, {'site_id': 21, 'x': 671, 'y': 823, 'radius': 84, 'gold': -1, 'maxMineSize': -1, 'structure_type': -1, 'owner': -1, 'param_1': -1, 'param_2': -1}, {'site_id': 22, 'x': 781, 'y': 616, 'radius': 60, 'gold': -1, 'maxMineSize': -1, 'structure_type': -1, 'owner': -1, 'param_1': -1, 'param_2': -1}, {'site_id': 23, 'x': 1139, 'y': 384, 'radius': 60, 'gold': -1, 'maxMineSize': -1, 'structure_type': 1, 'owner': 1, 'param_1': 304, 'param_2': 316}]
+sitesX = [{'x': 252, 'y': 252, 'owner': 0, 'unit_type': -1, 'health': 60}, {'x': 1668, 'y': 748, 'owner': 1, 'unit_type': -1, 'health': 60}, {'x': 252, 'y': 252, 'owner': 0, 'unit_type': -1, 'health': 60}, {'x': 1668, 'y': 748, 'owner': 1, 'unit_type': -1, 'health': 60}, {'x': 252, 'y': 252, 'owner': 0, 'unit_type': -1, 'health': 60}, {'x': 1610, 'y': 762, 'owner': 1, 'unit_type': -1, 'health': 60}, {'x': 310, 'y': 238, 'owner': 0, 'unit_type': -1, 'health': 60}, {'x': 1585, 'y': 768, 'owner': 1, 'unit_type': -1, 'health': 60}, {'x': 335, 'y': 232, 'owner': 0, 'unit_type': -1, 'health': 60}]
 
 
 class TestStringMethods(unittest.TestCase):
@@ -8,6 +16,21 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(0, cg.dist4(1, 2, 1, 2))
         a = {'x': 1, 'y': 2}
         self.assertEqual(0, cg.dist(a, a))
+
+    @patch('cg.print', create=True)
+    def test_angry(self, print_):
+        cg.Angry().get_strategy(sitesD, objectsD)
+        self.assertEqual(print_.call_count, 2)
+
+    @patch('cg.print', create=True)
+    def test_suicide(self, print_):
+        cg.Suicide().get_strategy(sitesD, objectsD)
+        self.assertEqual(print_.call_count, 2)
+
+    @patch('cg.print', create=True)
+    def test_dts(self, print_):
+        cg.DTS().get_strategy(sitesD, objectsD)
+        self.assertEqual(print_.call_count, 2)
 
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
